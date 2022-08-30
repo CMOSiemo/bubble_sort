0f3fc0a4f9410e31024c0993b7f24d256b239ad4
Claudio Martínez Ortiz <46463001+CMOSiemo@users.noreply.github.com>
2022-08-30 13:33:08 -0400

    v 1.02

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,12 @@
-#Copyright (c) 2022 Claudio Martínez Ortiz
+# frozen_string_literal: true
+
+# Copyright (c) 2022 Claudio Martínez Ortiz
 
 def sorted?(array)
-
-  #Method that checks if the array is sorted
+  # Method that checks if the array is sorted
   holder = false
 
-  for value in 0..(array.size - 2)
-
+  (0..(array.size - 2)).each do |value|
     if array[value] > array[value + 1]
 
       holder = false
@@ -15,39 +15,39 @@ def sorted?(array)
     else holder = true
 
     end
-
   end
 
   holder
-
 end
 
+def swap(array, value)
+  holder = array[value]
+  array[value] = array[value + 1]
+  array[value + 1] = holder
+end
 
 def bubble_sort(array)
+  # Checks if sorted
+  until sorted?(array)
 
-  #Checks if sorted
-  until sorted?(array) do
-   
     counter = 0
 
-    #Swap cycle
-    for value in 0..(array.size - 2 - counter)
+    # Swap cycle
+    (0..(array.size - 2 - counter)).each do |value|
+      next unless array[value] > array[value + 1]
 
-      if array[value] > array[value + 1]
-
-        holder = array[value]
-        array[value] = array[value + 1]
-        array[value + 1] = holder
-
-      end
-
+      swap(array, value)
     end
 
-    #Optimization
+    # Optimization
     counter += 1
 
   end
 
   array
-
 end
+
+trial = [4, 3, 78, 5 , 27, -1, 7 , 3, 0, 72, 84, 32.5]
+p trial
+bubble_sort(trial)
+p trial
